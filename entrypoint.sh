9fa12cd3e6a8725ed48c4907ab297faeb403cd15
Esolitos <esolitos@gmail.com>
2019-07-21 21:39:11 +0200

    Expand arguments on useradd entry point.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,9 @@
 USER_ID=${LOCAL_USER_ID:-9001}
 
 echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+useradd --shell "/bin/bash" --non-unique --comment "docker2local" --create-home --user-group --uid $USER_ID "user"
 
+echo "Ensureing correct ownership for mount points."
 chown -vR $USER_ID:$USER_ID /var/app /mnt/data /mnt/config
 
 export HOME=/home/user
